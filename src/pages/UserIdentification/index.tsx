cf83004c42eb922408d4f6185b484e33ef98c934
Willina <wsilvadev@gmail.com>
2021-04-25 15:34:06 -0300

    fit(pages): use routes to UserIdentification

--- src/pages/UserIdentification/index.tsx
+++ src/pages/UserIdentification/index.tsx
@@ -1,41 +1,72 @@
-import React, { useState } from 'react';
-import {SafeAreaView, View,Text, TextInput, KeyboardAvoidingView, Platform} from 'react-native';
+import React, { useState } from "react";
+import {
+  SafeAreaView,
+  View,
+  Text,
+  TextInput,
+  KeyboardAvoidingView,
+  Platform,
+  TouchableWithoutFeedback,
+  Keyboard,
+} from "react-native";
+import { useNavigation } from "@react-navigation/native";
 
-import styles from'./style'
-import Button from '../../components/Button'
-import colors from '../../styles/colors';
+import styles from "./style";
+import Button from "../../components/Button";
+import colors from "../../styles/colors";
 
-export default function UserIdentification (){
-    const [isFocused, setIsFocused] = useState(false)
-    const [isFilled, setIsFilled] = useState(false);
-    const [name, setName] = useState<string>()
-    const handleInputBlur =()=> {
-        setIsFocused(false);
-        setIsFilled(!!name)
-    }
-    const handleInputFocus = ()=> {
-        setIsFocused(true)
-    }
-    const handleInputChange = (value: string) => {
-        setIsFilled(!!value)
-        setName(value)
-    }
-    return (
-        <SafeAreaView style={styles.container}>
-            <KeyboardAvoidingView style={styles.container} behavior ={Platform.OS === 'ios' ? 'padding': 'height'}>
-            <View style={styles.content}>
-                <View style={styles.form}>
-                    <View style={styles.header}>
-                    <Text style={styles.emoji}>{!isFocused ? '😏': '😎'}</Text>
-                    <Text style={styles.title}>Como podemos {"\n"} chamar você?</Text>
-                    </View>
-                    <TextInput placeholder="Digite um nome" style={[styles.input, (isFilled || isFocused)  &&{borderColor: colors.green}]} onBlur={handleInputBlur} onFocus={handleInputFocus} onChangeText={handleInputChange}/>
-                    <View style={styles.footer}>
-                    <Button title='Confirmar'/>
-                    </View>
-                </View>
+export default function UserIdentification() {
+  const [isFocused, setIsFocused] = useState(false);
+  const [isFilled, setIsFilled] = useState(false);
+  const [name, setName] = useState<string>();
+  const handleInputBlur = () => {
+    setIsFocused(false);
+    setIsFilled(!!name);
+  };
+  const handleInputFocus = () => {
+    setIsFocused(!isFilled);
+  };
+  const handleInputChange = (value: string) => {
+    setIsFilled(!!value);
+    setName(value);
+  };
+  const navigation = useNavigation();
+
+  function handleSubmit() {
+    navigation.navigate("Confirmation");
+  }
+  return (
+    <SafeAreaView style={styles.container}>
+      <KeyboardAvoidingView
+        style={styles.container}
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+      >
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+          <View style={styles.content}>
+            <View style={styles.form}>
+              <View style={styles.header}>
+                <Text style={styles.emoji}>{!isFocused ? "😏" : "😎"}</Text>
+                <Text style={styles.title}>
+                  Como podemos {"\n"} chamar você?
+                </Text>
+              </View>
+              <TextInput
+                placeholder="Digite um nome"
+                style={[
+                  styles.input,
+                  (isFilled || isFocused) && { borderColor: colors.green },
+                ]}
+                onBlur={handleInputBlur}
+                onFocus={handleInputFocus}
+                onChangeText={handleInputChange}
+              />
+              <View style={styles.footer}>
+                <Button title="Confirmar" onPress={handleSubmit} />
+              </View>
             </View>
-            </KeyboardAvoidingView>
-        </SafeAreaView>
-    )
+          </View>
+        </TouchableWithoutFeedback>
+      </KeyboardAvoidingView>
+    </SafeAreaView>
+  );
 }
